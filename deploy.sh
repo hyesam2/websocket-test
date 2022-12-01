522a8c896b0cef8c27916f9fdd46ac607cbaede0
hyesam2 <85204502+hyesam2@users.noreply.github.com>
2022-12-02 00:09:09 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 sudo apt update
 sudo apt install npm -y
 echo "npm install complete"
-npm install bufferutil@^4.0.1 -g
+sudo npm install bufferutil@^4.0.1 -g
 echo "bufferutil install complete"
-npm install utf-8-validate@^5.0.2 -g
+sudo npm install utf-8-validate@^5.0.2 -g
 echo "utf8 install complete"
-npm install express -g
+sudo npm install express -g
 echo "express install complete"
-npm install ws -g
+sudo npm install ws -g
 echo "ws install complete"
 node app.js
